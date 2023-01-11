28f1c8278e13a73a2da2cb72c261489de5629436
Arthur Nunes <arthurnunesc@proton.me>
2023-01-11 21:58:10 +0100

    Legacy code gone

--- main.py
+++ main.py
@@ -23,22 +23,6 @@ word_in_arabic_without_diacritics = araby.strip_diacritics(word)
 word_in_arabic_with_diacritics_inverted = get_display(word)
 word_in_arabic_without_diacritics_inverted = arabic_reshaper.reshape(get_display((word_in_arabic_without_diacritics)))
 
-# LEGACY CODE LMAO
-# word = soup.find("div", class_="word").find("p").find_all("span")
-
-# if str(soup.find("p"))[33:37] == "verb":
-#    word_in_arabic_without_diacritics = formatArabicSentences(word[0].string)
-#    word_in_arabic_with_diacritics = word[0].string [::-1]
-#    word_in_arabic_with_diacritics_inverted = word[0].string
-#    word_in_arabic_without_diacritics_inverted = araby.strip_diacritics(word[0].string)
-#    word_pronunciation = word[1].string
-# elif str(soup.find("p"))[14:18] == "word":
-#    word_in_arabic_without_diacritics = formatArabicSentences(word[1].string)
-#    word_in_arabic_with_diacritics = word[1].string [::-1]
-#    word_in_arabic_with_diacritics_inverted = word[1].string
-#    word_in_arabic_without_diacritics_inverted = araby.strip_diacritics(word[1].string)
-#    word_pronunciation = word[0].string 
-
 # Fetches word meaning and declension and separates them in two variables
 word_meaning_and_declension = soup.select_one("div.word-declension-info-text").small.get_text()
 word_meaning_and_declension = word_meaning_and_declension.split(" – ")
@@ -50,12 +34,6 @@ if soup.find("div", class_="word-sound") != None:
    word_audio = soup.find("div", class_="word-sound").find("amp-audio")['src']
 else:
    word_audio = "no word audio"
-
-# LEGACY CODE LMAO
-# if "Part of speech: " in str(soup.find("div", class_="relation")):
-#    word_part_of_speech_and_pattern = soup.find("div", class_="relation").find("div")
-# elif "Part of speech: " in str(soup.find("div", class_="relation").next_sibling):
-#    word_part_of_speech_and_pattern = soup.find("div", class_="relation").next_sibling.find("div")
 
 # Fetches word part of speech and pattern
 word_part_of_speech_and_pattern = soup.select_one("div.similar-info")
@@ -92,14 +70,14 @@ else:
 # image
 
 # Printing everything on the terminal
-# print("arabic without diacritics:            ", word_in_arabic_without_diacritics)
-# print("arabic with diacritics:               ", word_in_arabic_with_diacritics)
-# print("arabic without diacritics(inverted):  ", word_in_arabic_without_diacritics_inverted)
-# print("arabic with diacritics(inverted):     ", word_in_arabic_with_diacritics_inverted)
-# print("pronunciation:                        ", word_pronunciation)
-# print("meaning:                              ", word_meaning)
-# print("declension:                           ", word_declension)
-# print("audio link:                           ", word_audio)
-# print("part of speech:                       ", word_part_of_speech)
-# print("pattern:                              ", word_pattern)
-# print("arabic.fi link:                       ", word_arabicfi_link)
+print("arabic without diacritics:            ", word_in_arabic_without_diacritics)
+print("arabic with diacritics:               ", word_in_arabic_with_diacritics)
+print("arabic without diacritics(inverted):  ", word_in_arabic_without_diacritics_inverted)
+print("arabic with diacritics(inverted):     ", word_in_arabic_with_diacritics_inverted)
+print("pronunciation:                        ", word_pronunciation)
+print("meaning:                              ", word_meaning)
+print("declension:                           ", word_declension)
+print("audio link:                           ", word_audio)
+print("part of speech:                       ", word_part_of_speech)
+print("pattern:                              ", word_pattern)
+print("arabic.fi link:                       ", word_arabicfi_link)
